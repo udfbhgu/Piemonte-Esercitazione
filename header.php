e91363eb9c86125ea85ebe750e3188ef5651909b
Gioele Battaglin <gioele.battaglin9@gmail.com>
2017-06-13 17:47:15 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,40 @@
+</!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>CV Lorenzo Carraro</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		
+		<!-- Custom CSS -->
+		<link href="css/custom.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<header>
+    		<nav class="navbar navbar-default" style="display: block; padding-left: 0px; padding-right: 10px; border-style: double;">
+        		<div class="container-fluid">
+          			<div class="navbar-header">
+          				<a class="navbar-brand" href="piemonte.html">
+							<ul class="nav navbar-nav">
+            					<a href="piemonte.html">Piemonte</a>
+        						<a href="alpi.html">Alpi</a>
+           						<a href="novara.html">Novara</a>
+           						<a href="lago-maggiore.html">Lago Maggiore</a>
+          					</ul>
+          				</a>
+          			</div>
+          		</div>
+      		</nav>
+		</header>
